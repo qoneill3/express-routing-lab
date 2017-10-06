2b90951e828758c74e86d9c52eb40be054c25fad
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2017-10-05 20:57:03 -0600

    finishes all routes

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,3 +1,4 @@
+'use strict';
 //Did you use npm install to
 //add all these packages
 //to our project?
@@ -6,12 +7,14 @@ var app = express();
 var bodyParser = require('body-parser');
 
 // How do we 'require' the candyRouter file?
-var candyRouter;
+var candyRouter = require('./candyRouter.js');
 
 app.use(bodyParser.json());
 
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
+
+app.use('/candies', candyRouter);
 
 app.listen(3000);
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -2,16 +2,69 @@ var express = require('express');
 var bodyParser = require('body-parser');
 router = express.Router();
 
-var candies;
+var candies = [
+	{"id":1,"name":"Chewing Gum","color":"Red"},
+	{"id":2,"name":"Pez","color":"Green"},
+	{"id":3,"name":"Marshmallow","color":"Pink"},
+	{"id":4,"name":"Candy Stick","color":"Blue"}
+];
 
 //What would need to go into candies
 //in order to pass our first test?
 
+
+//Get the Candies
 router.get('/', function(req,res) {
 	// What would go here? 
 	// Hint: we want all candies in JSON format
+	res.json(candies);
 });
 
 // Fill out the rest of the routes here
 
+
+//Get ONE candy
+router.get('/:id', (req, res) => {
+	var myCandy = candies.filter((element) => {
+		return element.id == req.params.id;
+	});
+	res.json(myCandy[0]);
+});
+
+//Add a candy
+router.post('/', function(req, res) {
+	candies.push(req.body);
+	res.json(candies);
+
+});
+
+
+//Update
+router.put('/:id', function(req, res) {
+	var newCandy =req.params.id;
+	for (var i= 0; candies.length; i++) {
+		if (dnewCandy ==candies[i].id) {
+			candies[i].name = req.body.name;
+			candies[i].color.body.color;
+		}
+
+	}
+	res.send("Updated!");
+});
+
+//Delete
+router.delete('/:id', function(req, res) {
+	var deleteCandy = req.params.id;
+	for (var i = 0; i < candies.length; i++) {
+		if (deleteCandy == candies[i].id) {
+			candies.pop(candies[i]);
+		}
+	}
+	res.send("Deleted!");
+});
+
+
+
 module.exports = router;
+
+
